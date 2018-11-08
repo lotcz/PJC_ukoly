ed84d4061a6872c57b453c75a0efff52fc2218a6
Karel Zavadil <mojemejly@centrum.cz>
2018-11-08 19:36:49 +0100

    while

--- 1-FMT_IO/small1.cpp
+++ 1-FMT_IO/small1.cpp
@@ -1,16 +1,37 @@
 #include "small1.hpp"
 
+/**
+* Načtení matice ze vstupu.
+* Jen spočítá řádky a slouce matice.
+*/
 std::pair<int, int> parse_matrix(std::istream& in){
-    return{ 0,0 };
+    int rows = 0;
+    int cols = 0;
+    char chr;
+    char last_chr;
+
+    while (!in.eof()) {
+      in >> chr;
+    }
+    //std::invalid_argument
 }
 
-void print_table(std::ostream& out, const std::vector<std::string>& vec) {    
+/**
+* Výpis tabulky na výstup
+*/
+void print_table(std::ostream& out, const std::vector<std::string>& vec) {
 }
 
+/**
+* Verifikace formátovaného vstupu
+*/
 bool validate_line(const std::string& str) {
     return true;
 }
 
+/**
+* Porovnání dlouhých čísel
+*/
 std::string max_number(std::istream& in) {
     return {};
 }
