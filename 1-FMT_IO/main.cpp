8afebf94d5e675fdfde710dbfc9d399c835da4c9
Karel Zavadil <mojemejly@centrum.cz>
2018-11-09 13:47:34 +0100

    parse_matrix

---
+++ 1-FMT_IO/main.cpp
@@ -0,0 +1,25 @@
+#include "small1.hpp"
+#include <sstream>
+#include <iostream>
+
+void output_value(std::string name, double value) {
+  std::cout << name;
+  std::cout << ": ";
+  std::cout << value;
+  std::cout << "\n";
+}
+
+int main() {
+  std::stringstream str;
+  std::string s =
+  R"f(1 2		3 4
+  5		6		7 8
+  9 10 12 13
+  13 14		15 33
+  16 17	22	11
+  )f";
+  str.str(s);
+  auto temp = parse_matrix(str);
+  output_value("first", temp.first);
+  output_value("second", temp.second);
+}
