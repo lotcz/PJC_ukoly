ea661578e433e3fec70adaff4ea6e81c658d608f
Karel Zavadil <mojemejly@centrum.cz>
2018-11-13 04:34:36 +0100

    ??

--- 2-FLATSET/flatset.hpp
+++ 2-FLATSET/flatset.hpp
@@ -10,9 +10,24 @@ class flat_set {
 
     typedef typename std::vector<T>::iterator my_iterator;
 
+    bool value_equals(T v1, T v2) {
+      return (!(this->m_comparator(v1, v2) || (this->m_comparator(v2, v1))));
+      //return false;
+    }
+
     my_iterator find_value_iterator(T value) {
       my_iterator it = this->begin();
       while (it != this->end() && (this->m_comparator(*it, value))) {
+        ++it;
+      }
+      return it;
+    }
+
+    typedef typename std::vector<T>::const_iterator my_const_iterator;
+
+    my_const_iterator find_value_const_iterator(T value) {
+      my_const_iterator it = this->cbegin();
+      while (it != this->cend() && (this->m_comparator(*it, value))) {
         ++it;
       }
       return it;
@@ -23,7 +38,7 @@ class flat_set {
       if (it == this->end()) {
         return make_pair(this->m_values.insert(it, value), true);
       } else {
-        if (*it == value) {
+        if (this->value_equals(*it, value)) {
           return make_pair(it, false);
         } else {
           return make_pair(this->m_values.insert(it, value), true);
@@ -38,15 +53,15 @@ class flat_set {
       }
     }
 
-    void from_flat_set(flat_set fs) {
-      this->from_iterator(fs.begin(), fs.end());
+    void from_flat_set(const flat_set* fs) {
+      this->from_iterator(fs->begin(), fs->end());
     }
 
   public:
     typedef T value_type;
     typedef typename std::vector<T>::size_type size_type;
     typedef my_iterator iterator;
-    typedef typename std::vector<T>::const_iterator const_iterator;
+    typedef my_const_iterator const_iterator;
 
     flat_set() = default;
 
@@ -55,22 +70,22 @@ class flat_set {
     }
 
     flat_set(flat_set const& rhs) {
-      this->from_flat_set(rhs);
+      this->from_flat_set(&rhs);
     }
 
     flat_set(flat_set && rhs) {
-      this->from_flat_set(rhs);
+      this->from_flat_set(&rhs);
     }
 
     flat_set& operator=(flat_set const& rhs) {
       this->clear();
-      this->from_flat_set(rhs);
+      this->from_flat_set(&rhs);
       return *this;
     }
 
     flat_set& operator=(flat_set && rhs) {
       this->clear();
-      this->from_flat_set(rhs);
+      this->from_flat_set(&rhs);
       return *this;
     }
 
@@ -105,14 +120,6 @@ class flat_set {
       this->from_iterator(first, last);
     }
 
-    // Erase overloads
-    // Deletes element pointed-to by i, returns iterator to the next element
-    iterator erase(const_iterator i);
-    // Deletes elements in range [first, last), returns iterator to the next element
-    iterator erase(const_iterator first, const_iterator last);
-    // Deletes element equal to key if it is present, returns how many elements were deleted
-    size_type erase(value_type const& key);
-
     // Iterator member functions
     iterator begin() noexcept {
       return this->m_values.begin();
@@ -139,11 +146,11 @@ class flat_set {
     }
 
     bool empty() const {
-     return (this->size() == 0);
+      return (this->size() == 0);
     }
 
     size_type size() const {
-     return this->m_values.size();
+      return this->m_values.size();
     }
 
     size_type capacity() const {
@@ -151,7 +158,7 @@ class flat_set {
     }
 
     void reserve(size_type c) {
-
+      this->m_values.reserve(c);
     }
 
     void clear() {
@@ -160,29 +167,124 @@ class flat_set {
 
     // Lookup member functions
     // Returns iterator to element equivalent to v, or an end iterator if such element is not present
-    iterator find(T const& v);
-    const_iterator find(T const& v) const;
+    iterator find(T const& v) {
+      my_iterator it = this->find_value_iterator(v);
+      if (it == this->end()) {
+        return it;
+      } else {
+        if (this->value_equals(*it, v)) {
+          return it;
+        } else {
+          return this->end();
+        }
+      }
+    }
+
+    const_iterator find(T const& v) const {
+      my_const_iterator it = this->find_value_const_iterator(v);
+      if (it == this->cend()) {
+        return it;
+      } else {
+        if (this->value_equals(*it, v)) {
+          return it;
+        } else {
+          return this->cend();
+        }
+      }
+    }
+
+    // Erase overloads
+    // Deletes element pointed-to by i, returns iterator to the next element
+    iterator erase(const_iterator i) {
+      return this->m_values.erase(i);
+    }
+
+    // Deletes elements in range [first, last), returns iterator to the next element
+    iterator erase(const_iterator first, const_iterator last) {
+      return this->m_values.erase(first, last);
+    }
+
+    // Deletes element equal to key if it is present, returns how many elements were deleted
+    size_type erase(value_type const& key) {
+      iterator it = this->find(key);
+      size_type size = 0;
+      while (it != this->end() && (*it == key)) {
+        it = this->erase(it);
+        size++;
+      }
+      return size;
+    }
 
     // Returns iterator to first element that is not less than t, end iterator if no such element is present
-    iterator lower_bound(T const& t);
-    const_iterator lower_bound(T const& t) const;
+    iterator lower_bound(T const& t) {
+      return this->find_value_iterator(t);
+    }
+
+    const_iterator lower_bound(T const& t) const {
+      return this->find_value_const_iterator(t);
+    }
 
     // Returns iterator to first element that is greater than t, end iterator if no such element is present
-    iterator upper_bound(T const& t);
-    const_iterator upper_bound(T const& t) const;
+    iterator upper_bound(T const& t) {
+      my_iterator it = this->find_value_iterator(t);
+      while (it != this->end() && (*it == t)) {
+        ++it;
+      }
+      return it;
+    }
 
-    void swap(flat_set& o);
+    const_iterator upper_bound(T const& t) const {
+      my_const_iterator it = this->find_value_const_iterator(t);
+      while (it != this->cend() && (*it == t)) {
+        ++it;
+      }
+      return it;
+    }
+
+    void swap(flat_set& o) {
+      flat_set f3;
+      f3 = o;
+      o.clear();
+      for (iterator it = this->begin(); it != this->end(); ++it) {
+        o.insert(*it);
+      }
+      this->clear();
+      this->from_flat_set(&f3);
+    }
 
 };
 
-/*
 template <typename T, typename Comparator>
-bool operator==(flat_set const& a, flat_set const& b);
-bool operator!=(flat_set const& a, flat_set const& b);
-bool operator<(flat_set const& a, flat_set const& b);
-bool operator<=(flat_set const& a, flat_set const& b);
-bool operator>=(flat_set const& a, flat_set const& b);
-bool operator>(flat_set const& a, flat_set const& b);
-*/
+bool operator==(flat_set<T, Comparator> const& a, flat_set<T, Comparator> const& b) {
+  return false;
+}
+
+template <typename T, typename Comparator>
+bool operator!=(flat_set<T, Comparator> const& a, flat_set<T, Comparator> const& b) {
+  return false;
+}
+
+template <typename T, typename Comparator>
+bool operator<(flat_set<T, Comparator> const& a, flat_set<T, Comparator> const& b) {
+  return false;
+}
+
+template <typename T, typename Comparator>
+bool operator<=(flat_set<T, Comparator> const& a, flat_set<T, Comparator> const& b) {
+  return false;
+}
+
+template <typename T, typename Comparator>
+bool operator>=(flat_set<T, Comparator> const& a, flat_set<T, Comparator> const& b) {
+  return false;
+}
+
+template <typename T, typename Comparator>
+bool operator>(flat_set<T, Comparator> const& a, flat_set<T, Comparator> const& b) {
+  return false;
+}
+
 template <typename T>
-void swap(flat_set<T> const& a, flat_set<T> const& b);
+void swap(flat_set<T> const& a, flat_set<T> const& b) {
+  a.swap(b);
+}
--- 2-FLATSET/main.cpp
+++ 2-FLATSET/main.cpp
@@ -12,15 +12,35 @@ void output_value(std::string name, T value) {
 }
 
 template <typename T>
+void output_iterator(std::string name, T start, T end) {
+  output_value("name", name);
+  for (auto it = start; it != end; it++) {
+    std::cout << *it << ",";
+  }
+  std::cout << "\n";
+}
+
+template <typename T, typename Comparator>
+void output_set(std::string name, flat_set<T, Comparator>* fs) {
+  output_iterator(name, fs->begin(), fs->end());
+}
+
+template <typename T>
 flat_set<T> make_flat_set(std::vector<T> const &v) {
   return flat_set<T>(v.begin(), v.end());
 }
 
 int main() {
-  auto l = make_flat_set<int>({ 1, 4, 8, 2, 3, 12, 4, 5, 12, -1 });
-  output_value("test const empty", l.empty());
-  output_value("test const size", l.size());
-  for (auto it = l.begin(); it != l.end(); ++it) {
-    std::cout << *it << ",";
+  try {
+
+    flat_set<int, std::greater<int>> fs;
+    std::vector<int> elems = {5, 0, 0, 1, 9, 2};
+    fs.insert(elems.begin(), elems.end());
+    output_value("size", fs.size());
+    output_set("set", &fs); //0, 1, 2, 5, 9
+
+
+  } catch (std::exception e) {
+    output_value("ex:", e.what());
   }
 }
