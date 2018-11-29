66b506dd1c382d8927f16aca0470af6a07f6ec98
Karel Zavadil <mojemejly@centrum.cz>
2018-11-29 02:37:47 +0100

    started working on Trie

---
+++ 3-TRIE/trie.cpp
@@ -0,0 +1,27 @@
+#include "trie.hpp"
+
+bool insert(trie& trie, const std::string& str) {
+    return true;
+}
+
+bool contains(const trie& trie, const std::string& str) {
+    return true;
+}
+
+void insert_all(trie& trie, const std::vector<std::string>& items) {}
+
+void init(trie& trie) {}
+
+bool erase(trie& trie, const std::string& str) {
+    return true;
+}
+
+void deallocate(trie& trie) {}
+
+size_t size(const trie& trie) {
+    return 0;
+}
+
+bool empty(const trie& trie) {
+    return true;
+}
